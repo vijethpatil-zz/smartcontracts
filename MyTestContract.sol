a7474ac3e9920b12f059b6b4a4927184e2a69cb6
vijethpatil <vijeth.patil@gmail.com>
2017-05-27 17:38:56 -0700

    Update MyTestContract.sol

--- MyTestContract.sol
+++ MyTestContract.sol
@@ -4,14 +4,16 @@ contract MyTestContract {
     uint valueX;
     
     // My constructor
-    function MyTestContract() {
+    function MyTestContract() payable {
        // init with value 100 everytime
        valueX = 100;
     }
     
     function setValueX(uint val) {
         // overriding value
-        valueX = val;
+        
+        uint aftercommision = calculateCommision(val);
+        valueX = aftercommision;
     }
     
     function  getValueX() constant returns(uint) {
@@ -19,5 +21,27 @@ contract MyTestContract {
         return valueX;
     }
     
+    function calculateCommision(uint val) constant returns(uint) {
+        return (val*99)/100;
+    }
     
+    function transferValue(uint val, address to)  returns(uint) {
+        uint targetCurrentValue = to.balance;
+        if(this.balance > val) {
+            to.transfer(val);
+            valueX = valueX - val;
+        }
+        
+        valueX = valueX - 1990;
+        targetCurrentValue = to.balance;
+        return targetCurrentValue;
+    }
+    
+    function myBalance() constant returns(uint) {
+        return this.balance;
+    }
+    
+    function() payable {
+        
+    }
 }
